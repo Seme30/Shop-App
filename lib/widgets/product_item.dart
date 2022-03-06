e97baf7956a2944824b7db159ed14c6938bf976b
Seme30 <semahegn.adugna1@gmail.com>
2022-03-06 12:29:38 +0300

    First Screen

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String productTitle;
+  final String imageUrl;
+
+  ProductItem(this.id, this.productTitle, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(imageUrl),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+          ),
+          title: Text(
+            productTitle,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
